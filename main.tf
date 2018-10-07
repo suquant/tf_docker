8eca19afe8d8d338281834bca8d8e661b658d519
Georgiy Kutsurua <g.kutsurua@gmail.com>
2018-10-07 18:55:31 +0400

    fixes

--- main.tf
+++ main.tf
@@ -73,15 +73,3 @@ output "public_ips" {
 
   depends_on = ["null_resource.docker"]
 }
-
-output "iptables_filter_chains" {
-  value = ["DOCKER", "DOCKER-ISOLATION"]
-
-  depends_on = ["null_resource.docker"]
-}
-
-output "iptables_nat_chains" {
-  value = ["DOCKER", "DOCKER-ISOLATION"]
-
-  depends_on = ["null_resource.docker"]
-}
